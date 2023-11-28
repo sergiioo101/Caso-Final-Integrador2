905e49493f9c03d187f381f8e60c3701c4e84699
sergiioo101 <smartros@myuax.com>
2023-11-28 12:20:13 +0100

    2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "jason.hpp"
-#include "jsonlib.hpp" // No estoy seguro de qué biblioteca es esta, asegúrate de tenerla en tu sistema
+#include "jason.cpp"
 
 enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
 
@@ -10,7 +9,7 @@ struct Entorno;
 
 class Variant {
 public:
-    typedef Variant(*proc_type) ( const std::vector<Variant>& );
+    typedef Variant (*proc_type)(const std::vector<Variant>&);
     typedef std::vector<Variant>::const_iterator iter;
     typedef std::map<std::string, Variant> map;
 
@@ -20,28 +19,33 @@ public:
     proc_type proc;
     Entorno* env;
 
-    Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
-    Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+    Variant(variant_type type = Symbol) : type(type), env(0), proc(0) {}
+    Variant(variant_type type, const std::string& val) : type(type), val(val), env(0), proc(0) {}
+    Variant(proc_type proc) : type(Proc), proc(proc), env(0) {}
 
     std::string to_string();
     std::string to_json_string();
     static Variant from_json_string(std::string json);
-    static Variant parse_json(jsonlib::Json job);  // Asegúrate de que esta es la definición correcta de la función
 };
 
 std::string Variant::to_string() {
     // Tu implementación aquí
+    return "";  // Asegúrate de agregar una implementación adecuada
 }
 
 std::string Variant::to_json_string() {
     // Tu implementación aquí
+    return "";  // Asegúrate de agregar una implementación adecuada
 }
 
 Variant Variant::from_json_string(std::string sjson) {
     // Tu implementación aquí
+    return Variant();  // Asegúrate de agregar una implementación adecuada
 }
 
-Variant Variant::parse_json(jsonlib::Json job) {
-    // Tu implementación aquí
+int main() {
+    // Tu código principal aquí
+
+    return 0;
 }
+
